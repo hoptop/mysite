078f2af745006e1cc3142ace93b2e2c663555ca2
unknown <>
2016-05-03 22:31:00 +0800

    准备构建网页

--- test.py
+++ test.py
@@ -1,15 +1,19 @@
-from flask import Flask
+from flask import Flask,render_template
 from flask import request
 from flask.ext.script import Manager
+from flask.ext.bootstrap import Bootstrap
+
 
 app = Flask(__name__)
 manager = Manager(app)
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index(): #view function
-    user_agent = request.headers.get('User-Agent')
-    return '<p> Your browser is %s</p>' %user_agent
+    # user_agent = request.headers.get('User-Agent')
+    return render_template('index.html')
     
 if __name__ == '__main__':
+    app.debug = True
     manager.run()
     
