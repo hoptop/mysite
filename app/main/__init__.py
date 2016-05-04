abe2848ef0626285c5782407c948f1f6fb32691d
unknown <>
2016-05-04 22:06:41 +0800

    编码问题

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from flask import Blueprint
 
 #参数为蓝本的名字和蓝本所在的包和模块
--- manage.py
+++ manage.py
@@ -1,4 +1,4 @@
-
+# -*- coding: utf-8 -*-
 import os
 from app import create_app,db
 from flask.ext.script import Manager, Shell
