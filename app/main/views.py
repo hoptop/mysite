4ae628302630c8745c490462d290a51e68a0d6cc
unknown <>
2016-05-04 21:18:20 +0800

    第一阶段

--- app/main/views.py
+++ app/main/views.py
@@ -1,9 +1,9 @@
-from datetime import datetime
+# from datetime import datetime
 from flask import render_template,session,redirect,url_for
 from . import main
-from .forms import NameForm
-from .. import db
-from ..models import User
+# from .forms import NameForm
+# from .. import db
+# from ..models import User
 
 # @main.route('/',methods=['GET','POST'])
 # def index():
